b5f3400bcfe444ad347e403001256d981b8d451c
Justin Schneck <jschneck@mac.com>
2016-04-06 20:41:48 -0400

    Updated config agent for comments and raise on redef

--- lib/nerves_system/config.ex
+++ lib/nerves_system/config.ex
@@ -1,10 +1,12 @@
 defmodule Nerves.System.Config do
-
+  # https://git.busybox.net/buildroot/tree/support/kconfig/merge_config.sh
   @t_merge [:"BR2_ROOTFS_OVERLAY"]
 
   def start do
-    Agent.start_link fn -> [] end, name: __MODULE__
+    Agent.start_link fn -> [comments: []] end, name: __MODULE__
   end
+
+  # TODO: Add an api call for stopping the agent
 
   def load(config_file) do
     config =
@@ -12,18 +14,31 @@ defmodule Nerves.System.Config do
       |> File.open!
       |> IO.stream(:line)
       |> Stream.map(& String.strip/1)
-      |> Stream.reject(& String.starts_with?(&1, "#"))
-      |> Stream.map(& String.split(&1, "="))
+    {comments, values} =
+      config
+      |> Enum.partition(& String.starts_with?(&1, "#"))
+    config =
+      values
+      |> Enum.map(& String.split(&1, "="))
       |> Keyword.new(& list_to_keyword/1)
+      |> Keyword.put(:comments, comments)
     Agent.update(__MODULE__, &(merge(config, &1)))
   end
 
   def dump do
     Agent.get(__MODULE__, &(&1))
-    |> Enum.map(fn ({k, v}) -> "#{k}=#{v}" end)
+    |> IO.inspect
+    |> Enum.map(fn
+      {:comments, c} -> Enum.join(c, "\n")
+      {k, v} = t -> "#{k}=#{v}"
+
+    end)
     |> Enum.reduce("", &(&2 <> "#{&1}\n"))
   end
 
+  def merge({:comments, v}, list) do
+    Keyword.put(list, :comments, list[:comments] ++ v)
+  end
   def merge({k, v}, list) when k in @t_merge do
     case Keyword.get(list, k) do
       nil -> Keyword.put(list, k, v)
@@ -31,7 +46,17 @@ defmodule Nerves.System.Config do
     end
   end
 
-  def merge({k, v}, list), do: Keyword.put_new(list, k, v)
+  def merge({k, v}, list) do
+    case Keyword.get(list, k) do
+      nil -> Keyword.put(list, k, v)
+      orig -> raise Nerves.System.Exception, message: """
+        Attempt to redefine key #{k} in defconfig
+        Original Value: #{orig}
+        Redefinition: #{v}
+      """
+    end
+
+  end
 
   def merge(l1, l2) when is_list(l1) and is_list(l2) do
     Enum.reduce(l1, l2, fn(line, l2) -> merge(line, l2) end)
