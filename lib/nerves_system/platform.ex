9a9602e99868f61bfe6db20d45f94dfebf0e2740
Justin Schneck <jschneck@mac.com>
2016-04-06 20:42:01 -0400

    enhance platform logic

--- lib/nerves_system/platform.ex
+++ lib/nerves_system/platform.ex
@@ -1,4 +1,8 @@
 defmodule Nerves.System.Platform do
+  @callback config(system :: Nerves.Env.Dep.t, dest :: binary) ::
+    :ok |
+    {:error, error :: term}
+
   @callback bootstrap() ::
     {:ok, archive :: binary} |
     {:error, response :: term}
@@ -8,5 +12,11 @@ defmodule Nerves.System.Platform do
       @behaviour Nerves.System.Platform
       alias Nerves.System.Platform
     end
+  end
+
+  def build_config(%Nerves.Env.Dep{config: config}) do
+    platform = config[:build_platform]
+    build_config =
+      Keyword.merge(platform.default_build_config, config[:build_config] || [])
   end
 end
--- lib/nerves_system/platforms/br.ex
+++ lib/nerves_system/platforms/br.ex
@@ -1,6 +1,20 @@
 defmodule Nerves.System.Platforms.BR do
   use Nerves.System.Platform
   alias Nerves.Env
+  alias Nerves.System.Config
+
+  require Logger
+
+  @dest "config"
+
+  def config(%Env.Dep{} = system, dest) do
+    build_config = Platform.build_config(system)
+    dest = Path.join(dest, build_config[:dest])
+    File.mkdir_p!(dest)
+    Logger.debug "Build Config: #{inspect build_config}"
+    copy_configs(system, build_config, dest)
+    assemble_defconfig(system, build_config, dest)
+  end
 
   def bootstrap do
     Env.deps_by_type(:system_platform)
@@ -8,5 +22,47 @@ defmodule Nerves.System.Platforms.BR do
     |> Map.get(:path)
     |> Path.join("nerves_env.exs")
     |> Code.require_file
+  end
+
+  def default_build_config do
+    [defconfig: "nerves_defconfig",
+     kconfig: "Config.in",
+     dest: @dest,
+     assets: []]
+  end
+
+  defp copy_configs(%Env.Dep{} = system, build_config, dest) do
+    ret = Path.join(system.path, build_config[:defconfig])
+    |> File.cp(Path.join(dest, build_config[:defconfig]))
+    Logger.debug "Copy Config: #{inspect dest}"
+    kconfig_path = Path.join(system.path, build_config[:kconfig])
+    if File.exists?(kconfig_path) do
+      File.cp(kconfig_path, dest)
+    else
+      Path.join(dest, "Config.in")
+      |> File.touch
+    end
+  end
+
+  defp assemble_defconfig(system, build_config, dest) do
+    system_defconfig =
+      dest
+      |> Path.join(build_config[:defconfig])
+
+    unless File.exists?(system_defconfig), do: raise """
+    System defconfig cannot be found at #{inspect system_defconfig}
+    """
+    Config.start
+    Config.load(system_defconfig)
+
+    Enum.each(Env.system_exts, fn(%{path: path, config: config}) ->
+      if config[:build_config] != nil do
+        if config[:build_config][:defconfig] != nil do
+          ext_defconfig = Path.join(path, config[:build_config][:defconfig])
+          Config.load(ext_defconfig)
+        end
+      end
+    end)
+    File.write!(system_defconfig, Config.dump)
   end
 end
