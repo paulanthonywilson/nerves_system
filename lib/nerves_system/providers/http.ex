6e435718a36f1d7a1f9e8c711539fa10bbcf4532
Justin Schneck <jschneck@mac.com>
2016-04-23 12:32:01 -0400

    short circuit :nocache

--- lib/nerves_system/providers/http.ex
+++ lib/nerves_system/providers/http.ex
@@ -18,23 +18,25 @@ defmodule Nerves.System.Providers.Http do
 
   defp result({:ok, %{status_code: status, body: body}}, _) when status in 200..299 do
     shell_info "System Downloaded"
-    body
+    {:ok, body}
   end
   defp result(_, []) do
-    raise "No mirror returned a result"
+    shell_info "No Available Mirrors"
+    {:error, :nocache}
   end
   defp result(_, mirrors) do
     shell_info "switching mirror"
     get(mirrors)
   end
 
-  defp unpack(tar, destination) do
+  defp unpack({:error, _} = error, _), do: error
+  defp unpack({:ok, tar}, destination) do
     shell_info "Unpacking System"
     tmp_path = Path.join(destination, ".tmp")
     File.mkdir_p!(tmp_path)
     tar_file = Path.join(tmp_path, "system.tar.xz")
     File.write(tar_file, tar)
-
+    
     System.cmd("tar", ["xf", "system.tar.xz"], cd: tmp_path)
     source =
       File.ls!(tmp_path)
